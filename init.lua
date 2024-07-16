8dc1bb8c105232dbff9b2d2299967c387474dc05
azeredo-e <eduardo.azeredo99@gmail.com>
2024-07-16 16:48:20 -0300

    First config test

---
+++ init.lua
@@ -0,0 +1,6 @@
+-- Load all config files
+
+require('config.options')
+require('config.keymaps')
+require('config.lazy')
+require('config.colours')  -- Theme at the end, to prevent overwrite by other plugins
---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,33 @@
+local mason_lspconfig = require("mason-lspconfig")
+local lspconfig = require("lspconfig")
+local common_on_attach = require("lsp.utils").common_on_attach
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+
+capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+
+local servers = {
+    "pylsp",
+    "julials",
+    "lua_ls",
+    -- "clangd", -- C
+    "rust_analyzer",
+    -- "hls", -- Haskell
+    "jsonls",
+    "html",
+    "tsserver",
+    "marksman", -- Markdown
+    -- "texlab", -- Latex
+}
+
+mason_lspconfig.setup({
+  ensure_installed = servers,
+})
+
+mason_lspconfig.setup_handlers({
+  function (server_name)
+    lspconfig[server_name].setup {
+      on_attach = common_on_attach,
+      capabilities = capabilities,
+    }
+  end
+})
